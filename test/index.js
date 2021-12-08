0eeac51eb32264f9768dc5329bfafe56e0a56cb3
phongpham <hoangphong24032000@gmail.com>
2021-12-08 10:23:05 +0700

    w3file

---
+++ test/index.js
@@ -0,0 +1,19 @@
+function Print(M, N) {
+    while (M > N) {
+        if (M < N) {
+            M = N - M
+        }
+
+        if (M > N) {
+            M = M - N
+        }
+
+        if (M = N) {
+            return M
+        }
+        return M
+    } 
+    
+}
+
+console.log(Print(100, 85));
